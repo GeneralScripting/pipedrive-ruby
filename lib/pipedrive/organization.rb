c019ebee79970d6266c8186aea3e49bc4ac97775
Jason Kay <jason@callpixels.com>
2013-02-07 01:55:05 -0500

    Add #persons to organization.

--- lib/pipedrive/organization.rb
+++ lib/pipedrive/organization.rb
@@ -2,6 +2,11 @@ module Pipedrive
   class Organization < Base
     include Deals
 
+    # TODO Rewrite this.
+    def persons
+      Person.all(get "#{resource_path}/#{id}/persons")
+    end
+
     class << self
 
       def find_or_create_by_name(name, opts={})
