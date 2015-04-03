97fac44f50177f588085a112fcfda6f3574ccbc3
Mo Dhaliwal <mo@skyrocket.is>
2015-04-03 14:19:09 -0700

    added delete activity endpoint

--- lib/pipedrive/activity.rb
+++ lib/pipedrive/activity.rb
@@ -1,4 +1,10 @@
 module Pipedrive
   class Activity < Base
+
+    def delete_activity activity_id
+      res = delete "#{resource_path}/#{id}", { :body => { :id => activity_id } }
+      res.success? ? nil : bad_response(res,activity_id)
+    end
+
   end
 end
