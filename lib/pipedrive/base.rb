6a701030043a5a68ffac0daa392ad1672169ffe0
MehreenT <mehreen.tahir@square63.com>
2017-02-07 00:59:02 +0500

    Base: Updated the conditions of pagination in retrival of entities

--- lib/pipedrive/base.rb
+++ lib/pipedrive/base.rb
@@ -89,7 +89,7 @@ module Pipedrive
         res = response || get(resource_path, options)
         if res.ok?
           data = res['data'].nil? ? [] : res['data'].map{|obj| new(obj)}
-          if get_absolutely_all && res['additional_data']['pagination'] && res['additional_data']['pagination'] && res['additional_data']['pagination']['more_items_in_collection']
+          if get_absolutely_all && res['additional_data'] && res['additional_data']['pagination'] && res['additional_data']['pagination']['more_items_in_collection']
             options[:query] = options[:query].merge({:start => res['additional_data']['pagination']['next_start']})
             data += self.all(nil,options,true)
           end
