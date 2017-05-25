a121eb2805f0d647f2b3c30a16a2491b46936c1e
Vincent <vincent@dowza.com>
2017-05-25 17:18:26 +0000

    Added organization getter in Activity ; cleaned up tests

--- test/helper.rb
+++ test/helper.rb
@@ -19,4 +19,45 @@ require 'pipedrive-ruby'
 
 class Test::Unit::TestCase
   include ShouldaContextLoadable
+  
+  def stub method, resource, body_file_name, request_body=nil
+    if method == :get
+      uri = "https://api.pipedrive.com/v1/#{resource}?api_token=some-token"
+      # Add request params to uri if any
+      unless request_body.nil?
+        uri += request_body.map{|k,v| "#{k}=#{v}"}.join('&')
+      end
+      # stub request
+      request_stubbed = stub_request(:get, uri)
+    else
+      request_stubbed = stub_request(method, "https://api.pipedrive.com/v1/#{resource}?api_token=some-token")
+    end
+    
+    request_stubbed.
+      with(headers: request_headers).
+      to_return(
+        status: 200,
+        body: File.read(File.join(File.dirname(__FILE__), "data", body_file_name)),
+        headers: response_headers
+      )
+  end
+  
+  def request_headers
+    {
+      'Accept'=>'application/json',
+      'Content-Type'=>'application/x-www-form-urlencoded',
+      'User-Agent'=>'Ruby.Pipedrive.Api'
+    }
+  end
+  
+  def response_headers
+    {
+      "server" => "nginx/1.2.4",
+      "date" => "Fri, 01 Mar 2013 14:01:03 GMT",
+      "content-type" => "application/json",
+      "content-length" => "1260",
+      "connection" => "keep-alive",
+      "access-control-allow-origin" => "*"
+    }
+  end
 end
--- test/test_pipedrive_person.rb
+++ test/test_pipedrive_person.rb
@@ -4,40 +4,27 @@ class TestPipedrivePerson < Test::Unit::TestCase
   def setup
     Pipedrive.authenticate("some-token")
   end
-
-  should "execute a valid person request" do
-    body = {
-      "email"=>["john@dope.org"],
-      "name"=>"John Dope",
-      "org_id"=>"404",
-      "phone"=>["0123456789"]
-    }
-
-    stub_request(:post, "https://api.pipedrive.com/v1/persons?api_token=some-token").
-      with(:body => body, :headers => {
-          'Accept'=>'application/json',
-          'Content-Type'=>'application/x-www-form-urlencoded',
-          'User-Agent'=>'Ruby.Pipedrive.Api'
-        }).
-      to_return(
-        :status => 200,
-        :body => File.read(File.join(File.dirname(__FILE__), "data", "create_person_body.json")),
-        :headers => {
-          "server" => "nginx/1.2.4",
-          "date" => "Fri, 01 Mar 2013 13:34:23 GMT",
-          "content-type" => "application/json",
-          "content-length" => "1164",
-          "connection" => "keep-alive",
-          "access-control-allow-origin" => "*"
-        }
-      )
-
-    person = ::Pipedrive::Person.create(body)
-
-    assert_equal "John Dope", person.name
-    assert_equal 404, person.org_id
-    assert_equal "john@dope.org", person.email.first.fetch("value")
-    assert_equal "0123456789", person.phone.first.fetch("value")
+  
+  context "create a person" do
+    setup do
+      body = {
+        "email"=>["john@dope.org"],
+        "name"=>"John Dope",
+        "org_id"=>"404",
+        "phone"=>["0123456789"]
+      }
+      
+      stub :post, "persons", "create_person_body.json", body
+      
+      @person = ::Pipedrive::Person.create(body)
+    end
+    
+    should "get a valid person" do
+      assert_equal "John Dope", @person.name
+      assert_equal 404, @person.org_id
+      assert_equal "john@dope.org", @person.email.first.fetch("value")
+      assert_equal "0123456789", @person.phone.first.fetch("value")
+    end
   end
 
   should "return bad_response on errors" do
